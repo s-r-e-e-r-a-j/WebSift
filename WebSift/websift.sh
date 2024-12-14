63ac8370bdd3ba693f4f287b4984e6329f93b7ab
Sreeraj <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2024-12-14 21:32:58 +0530

    Update websift.sh

--- WebSift/websift.sh
+++ WebSift/websift.sh
@@ -83,8 +83,8 @@ clear
 EOF
 
 echo -e "${Cyan}* Email And PhoneNumber Scraper Tool *\033[0m"
-echo -e "${BrightYellow}* GitHub:https://GitHub.com/s-r-e-e-r-a-j * \033[0m"
-echo -e "\n"
+echo -e "${BrightYellow}* GitHub:https://GitHub.com/s-r-e-e-r-a-j * \033[0m\n"
+
 }
 
 # Main function to handle input and validations
